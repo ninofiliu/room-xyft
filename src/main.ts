81cbe625cfba89bd9eb7cf3a6f8db256aaf12b97
Nino Filiu <nino.filiu@gmail.com>
2022-01-26 00:45:02 +0100

    import sequential distortio algorithm

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,46 @@
 import { getBpm, getKickProgression } from './bpm';
 import getSource from './getSource';
 
+import { addTexture, setTextureImage, webglSetup } from './webgl';
+
+const loadImage = async (src: string) => {
+  const image = new Image();
+  image.src = src;
+  await new Promise((r) => {
+    image.onload = r;
+  });
+  return image;
+};
+
+const crop = (
+  image: HTMLImageElement,
+  width: number,
+  height: number,
+  blur: number
+) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext('2d');
+  ctx.filter = `blur(${blur}px)`;
+  if (image.width / image.height > width / height) {
+    const sw = (image.height * width) / height;
+    const sx = (image.width - sw) / 2;
+    ctx.drawImage(image, sx, 0, sw, image.height, 0, 0, width, height);
+  } else {
+    const sh = (image.width * height) / width;
+    const sy = (image.height - sh) / 2;
+    ctx.drawImage(image, 0, sy, image.width, sh, 0, 0, width, height);
+  }
+  ctx.filter = '';
+  return ctx.getImageData(0, 0, width, height);
+};
+
 (async () => {
+  const offset = { x: 1.0, y: 1.0 };
+  const force = 1.3;
+  const blur = 0;
+
   const ac = new AudioContext();
   const source = await getSource(ac, null);
 
@@ -19,7 +58,46 @@ import getSource from './getSource';
   document.body.style.overflow = 'hidden';
   document.body.style.margin = '0';
   document.body.append(canvas);
-  const ctx = canvas.getContext('2d');
+
+  const { gl, program } = await webglSetup(
+    canvas,
+    new URL('./vertex.glsl', import.meta.url).href,
+    new URL('./fragment.glsl', import.meta.url).href
+  );
+
+  const srcImages = await Promise.all(
+    [...Array(9).keys()].map((i) => loadImage(`/images/${i}.jpg`))
+  );
+  const dstIDs = srcImages.map((img) => crop(img, width, height, blur));
+
+  addTexture(gl, 0, gl.getUniformLocation(program, 'u_image_0'));
+  addTexture(gl, 1, gl.getUniformLocation(program, 'u_image_1'));
+  addTexture(gl, 2, gl.getUniformLocation(program, 'u_offsets_0'));
+  addTexture(gl, 3, gl.getUniformLocation(program, 'u_offsets_1'));
+
+  let step = 0;
+  let srcStep = 0;
+  const maxSrcStep = 100;
+  let srcImage0 = srcImages[~~(Math.random() * srcImages.length)];
+  let srcImage1 = srcImages[~~(Math.random() * srcImages.length)];
+  let dstStep = 0;
+  const maxDstStep = 60;
+  let dstID0 = dstIDs[~~(Math.random() * dstIDs.length)];
+  let dstID1 = dstIDs[~~(Math.random() * dstIDs.length)];
+
+  const setNewSrc = () => {
+    srcImage0 = srcImage1;
+    srcImage1 = srcImages[~~(Math.random() * srcImages.length)];
+    setTextureImage(gl, 0, srcImage0);
+    setTextureImage(gl, 1, srcImage1);
+  };
+
+  const setNewDst = () => {
+    dstID0 = dstID1;
+    dstID1 = dstIDs[~~(Math.random() * dstIDs.length)];
+    setTextureImage(gl, 2, dstID0);
+    setTextureImage(gl, 3, dstID1);
+  };
 
   const animate = () => {
     analyser.getByteFrequencyData(fft);
@@ -29,6 +107,27 @@ import getSource from './getSource';
       .reduce((sum, e) => sum + e, 0);
     const kp = getKickProgression();
     const bpm = getBpm();
+
+    gl.uniform2f(
+      gl.getUniformLocation(program, 'u_offset'),
+      offset.x,
+      offset.y
+    );
+    gl.uniform1f(gl.getUniformLocation(program, 'u_force'), force);
+    gl.uniform1f(gl.getUniformLocation(program, 'u_time'), step / 60);
+
+    if (srcStep === 0) setNewSrc();
+    const srcMix = (srcStep / maxSrcStep) ** 4;
+    gl.uniform1f(gl.getUniformLocation(program, 'u_mix_src'), srcMix);
+    srcStep = (srcStep + 1) % maxSrcStep;
+
+    if (dstStep === 0) setNewDst();
+    const dstMix = dstStep / maxDstStep;
+    gl.uniform1f(gl.getUniformLocation(program, 'u_mix_dst'), dstMix);
+    dstStep = (dstStep + 1) % maxDstStep;
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    step++;
 
     requestAnimationFrame(animate);
   };
---
+++ src/webgl.ts
@@ -0,0 +1,53 @@
+const createShader = async (gl: WebGL2RenderingContext, type: number, url: string) => {
+  const resp = await fetch(url);
+  const source = await resp.text();
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(shader));
+  }
+  return shader;
+};
+
+export const webglSetup = async (canvas: HTMLCanvasElement, vertexUrl: string, fragmentUrl: string) => {
+  const gl = canvas.getContext('webgl2');
+  const vertexShader = await createShader(gl, gl.VERTEX_SHADER, vertexUrl);
+  const fragmentShader = await createShader(gl, gl.FRAGMENT_SHADER, fragmentUrl);
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw new Error(gl.getProgramInfoLog(program));
+  }
+  gl.useProgram(program);
+  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+  gl.enableVertexAttribArray(gl.getAttribLocation(program, 'a_position'));
+  gl.vertexAttribPointer(gl.getAttribLocation(program, 'a_position'), 2, gl.FLOAT, false, 0, 0);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    -1, -1,
+    -1, 1,
+    1, -1,
+    1, -1,
+    -1, 1,
+    1, 1,
+  ]), gl.STATIC_DRAW);
+  return { gl, program };
+};
+
+export const addTexture = (gl: WebGL2RenderingContext, nb: number, location: WebGLUniformLocation) => {
+  gl.activeTexture(gl.TEXTURE0 + nb);
+  gl.bindTexture(gl.TEXTURE_2D, gl.createTexture());
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.uniform1i(location, nb);
+};
+
+export const setTextureImage = (gl: WebGL2RenderingContext, nb: number, source: ImageBitmap | ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement) => {
+  gl.activeTexture(gl.TEXTURE0 + nb);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, source);
+};
